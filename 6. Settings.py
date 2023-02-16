169794e9d80ef5e6cac5cc87d5660ef1fcc630e4
Md. Touhidul Islam <36783173+touhidbd9952@users.noreply.github.com>
2023-02-16 12:14:25 +0900

    Update 6. Settings.py

--- 6. Settings.py
+++ 6. Settings.py
@@ -1,17 +1,23 @@
 settings.py
 ------------------------------
-#application
+
 INSTALLED_APPS = [
 		.
 		.
-	   'amphibious',
+	   'amphibious',  //your application register
+	   'django_cleanup.apps.CleanupConfig',  //old image delete
 	]
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+'DIRS':  [TEMPLATES_DIR,],
+	
 #css, js, font
-STATIC_URL  = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_URL  = '/static/'
 
 #Image
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL  = '/media/'
 
